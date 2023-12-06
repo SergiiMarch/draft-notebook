4ad6405f94c6e092b86fc3e50db25fda9801bab5
Sergii Marchenko <marikmarchelo@gmail.com>
2023-12-06 10:00:17 +0200

    add new commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+console.log(apartment);
+const values = [];
+// Change code below this line
+
+const keys = Object.keys(apartment);
+
+console.log(keys);
+// Перебери об'єкт apartment,
+// використовуючи метод Object.keys()
+// і цикл for...of.
+// Запиши у змінну keys масив ключів
+// власних властивостей об'єкта apartment,
+//  додай в масив values всі значення його властивостей
