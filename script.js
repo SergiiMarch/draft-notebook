f50e752002171caf6b3e1df4f1d674e52dabec00
Sergii Marchenko <marikmarchelo@gmail.com>
2023-12-20 21:37:42 +0200

    new task

--- script.js
+++ script.js
@@ -650,3 +650,27 @@
 // // в іншому випадку, відніми суму покупки з рахунку клієнта і
 // //  запиши у змінну message повідомлення: "You ordered <число> droids, you have <число> credits left".
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function checkStorage(available, ordered) {
+  let message;
+  // Change code below this line
+  if (ordered === 0) {
+    message = "There are no products in the order!";
+  } else if (ordered < available) {
+    message = "Your order is too large, there are not enough items in stock!";
+  } else {
+    ("The order is accepted, our manager will contact you");
+  }
+  // Change code above this line
+  return message;
+}
+console.log(checkStorage(100, 50));
+
+// available - доступна кількість товарів на складі
+// ordered - одиниць товару в замовленні
+// Використовуючи розгалуження, доповни код функції таким чином, що:
+
+// Якщо в замовленні ще немає товарів, тобто значення параметра ordered дорівнює 0,
+// у змінну message присвоюється рядок "There are no products in the order!".
+// Якщо товарів у замовленні більше, ніж доступно товарів на складі,
+// то у змінну message присвоюється рядок "Your order is too large, there are not enough items in stock!".
+// В іншому випадку у змінну message присвоюється рядок "The order is accepted, our manager will contact you".
