9a564d5b96947ea0b1eec7e4d04bccb37b90b395
Sergii Marchenko <marikmarchelo@gmail.com>
2023-12-22 21:10:43 +0200

    add task

--- script.js
+++ script.js
@@ -1116,14 +1116,22 @@
 // console.log(createArrayOfNumbers(29, 34));
 // console.log(createArrayOfNumbers(14, 17));
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-function filterArray(numbers, value) {
-  // Change code below this line
-  // Change code above this line
-}
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-// Напиши функцію filterArray(numbers, value),
-//  яка приймає масив чисел (параметр numbers) і
-//  повертає новий масив, в якому будуть тільки ті
-//  елементи масиву numbers, які більші за значення параметра value (число).
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   const resArray = [];
+//   // Change code above this line
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] > value) {
+//       resArray.push(numbers[i]);
+//     }
+//   }
+//   return resArray;
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// // Напиши функцію filterArray(numbers, value),
+// //  яка приймає масив чисел (параметр numbers) і
+// //  повертає новий масив, в якому будуть тільки ті
+// //  елементи масиву numbers, які більші за значення параметра value (число).
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
