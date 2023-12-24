38cfc8547764ae4513ce6ab71eff98e222f3985f
Sergii Marchenko <marikmarchelo@gmail.com>
2023-12-24 15:35:44 +0200

    add task

--- script.js
+++ script.js
@@ -1248,3 +1248,23 @@
 // //   чи присутнє в масиві array значення value, повертаючи true, якщо присутнє, і false в іншому випадку.
 // // При виконанні цього завдання в тілі функції includes() не можна використовувати метод масив.includes(значення).
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// // Chande code below this line
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
+// // Доповни функцію makeMessage таким чином,
+// // щоб вона очікувала другим параметром (параметр callback) колбек-функцію
+// //  і повертала її виклик.
+// //  Функція deliverPizza або makePizza буде передаватися як колбек
+// //  і очікувати аргументом ім'я готової піци, що доставляється.
+// console.log(makeMessage("Royal Grand", makePizza));
+// console.log(makeMessage("Ultracheese", deliverPizza));
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
