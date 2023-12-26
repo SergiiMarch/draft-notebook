e2880532cfd376746bf1e64fd6508d97c8dffc01
Sergii Marchenko <marikmarchelo@gmail.com>
2023-12-26 21:38:20 +0200

    add task

--- script.js
+++ script.js
@@ -1703,3 +1703,11 @@
 //   ])
 // );
 ///////////////////////////////////////////////////////////////////////////////////
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const evenNumbers = numbers.filter((number) => number % 2 === 0);
+// const oddNumbers = numbers.filter((number) => number % 2 !== 0);
+// console.log(evenNumbers);
+// console.log(oddNumbers);
+//////////////////////////////////////////////////////////////////////////////////
