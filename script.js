0ac292b3f76cf85fd97ece38921de9f822cec713
Sergii Marchenko <marikmarchelo@gmail.com>
2023-12-26 15:39:01 +0200

    add task

--- script.js
+++ script.js
@@ -1426,3 +1426,18 @@
 // // Change code above this line
 // console.log(calculateTotalPrice([12, 85, 37, 4]));
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Change code below this line
+// const filterArray = (numbers, value) => {
+//   const filteredNumbers = [];
+
+//   numbers.forEach((number) => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+//   // Change code above this line
+//   return filteredNumbers;
+// };
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
