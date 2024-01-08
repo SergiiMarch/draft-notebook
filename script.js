1c2b7aaaa2951027b5d84d8ef133f8d4b838232a
Sergii Marchenko <marikmarchelo@gmail.com>
2024-01-08 12:42:15 +0200

    add commit

--- script.js
+++ script.js
@@ -3279,3 +3279,82 @@ console.log(findDifference([2, 2, 3], [5, 7, 4, 1]));
 // // щоб він приймав один параметр - об'єкт з властивостями brand,
 // //  model і price. Деструктуризуй об'єкт в сигнатурі (підписі) конструктора.
 //////////////////////////////////////////////////////////////////////////////////////////////////
+// Change code above this line
+
+// class Storage {
+//   constructor(items) {
+//     this.items = items;
+//   }
+//   getItems() {
+//     return this.items;
+//   }
+//   addItem(newItem) {
+//     this.items.push(newItem);
+//   }
+//   removeItem(itemToRemove) {
+//     const idxItems = this.items.findIndex((item) => item === itemToRemove);
+//     if (!!~idxItems) {
+//       this.items.splice(idxItems, 1);
+//     }
+//   }
+// }
+
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// // Напиши клас Storage, який буде створювати
+// // об'єкти для управління складом товарів.
+// // Клас очікує тільки один аргумент - початковий масив товарів, який записується у властивість items об'єкта, що створюється.
+// // Оголоси наступні методи класу:
+// // getItems() - повертає масив поточних товарів у
+// // властивості items об'єкта, який викликає цей метод.
+// // addItem(newItem) - приймає новий товар newItem і
+// // додає його в масив товарів у властивості items об'єкта, який викликає цей метод.
+// // removeItem(itemToRemove) - приймає товар itemToRemove
+// // і видаляє його з масиву товарів у властивості items об'єкта, який викликає цей метод.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Change code above this line
+// class StringBuilder {
+//   constructor(initialValue) {
+//     this.value = initialValue;
+//   }
+//   getValue() {
+//     return this.value;
+//   }
+//   padEnd(str) {
+//     this.value += str;
+//   }
+
+//   padStart(str) {
+//     this.value = str + this.value;
+//   }
+
+//   padBoth(str) {
+//     this.value = str + this.value + str;
+//   }
+// }
+
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+// // Напиши клас StringBuilder, який приймає один параметр
+// // initialValue - довільний рядок, який записується у властивість value об'єкта, що створюється.
+// // Оголоси наступні методи класу:
+// // getValue() - повертає поточне значення властивості value.
+// // padEnd(str) - отримує параметр str (рядок)
+// // і додає його в кінець значення властивості value об'єкта, який викликає цей метод.
+// // padStart(str) - отримує параметр str (рядок)
+// // і додає його на початок значення властивості value об'єкта, який викликає цей метод.
+// // padBoth(str) - отримує параметр str (рядок)
+// //  і додає його на початок і в кінець значення властивості value об'єкта, який викликає цей метод.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
