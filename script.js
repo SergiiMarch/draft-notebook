e5f172c035e1628894fe955869ba53da081e245d
Sergii Marchenko <marikmarchelo@gmail.com>
2024-01-11 20:38:01 +0200

    add task

--- script.js
+++ script.js
@@ -3572,3 +3572,31 @@ console.log(findDifference([2, 2, 3], [5, 7, 4, 1]));
 // //  що передається. Якщо воно більше за MAX_PRICE,
 // //  сеттер нічого не робить, а якщо менше або дорівнює, то перезаписує ціну автомобіля.
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   // Change code below this line
+//   static checkPrice(price) {
+//     return this.#MAX_PRICE < price
+//       ? "Error! Price exceeds the maximum"
+//       : "Success! Price is within acceptable limits";
+//   }
+//   // Change code above this line
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+// // Додай класу Car публічний статичний метод
+// // checkPrice (price), що приймає ціну автомобіля.
+// // Метод повинен порівняти значення параметра price і
+// // приватної статичного властивості MAX_PRICE.
+// // Якщо ціна автомобіля перевищує максимальну,
+// // метод повинен повернути рядок "Error! Price exceeds the maximum".
+// // В іншому випадку метод повинен повернути рядок "Success! Price is within acceptable limits".
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
